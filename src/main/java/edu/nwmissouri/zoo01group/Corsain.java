2d6bcc61dcd80caf4977f7388918d6b334e660ba
Govind2003 <89623058+Govind2003@users.noreply.github.com>
2021-11-02 23:11:51 -0500

    Added Boat and corsain classes

---
+++ src/main/java/edu/nwmissouri/zoo01group/Corsain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo01group;
+
+/**
+ *
+ * @author S545524
+ */
+public class Corsain extends Boat {
+
+    public Corsain() {
+    }
+    
+}
