75653437b78f0d6b1f272b9e4d1614c9a93bf42b
bharathnw <msreebharath21@gmail.com>
2021-11-02 21:14:02 -0500

    modified Animal and App name

---
+++ src/main/java/edu/nwmissouri/zoo01group/Animal.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo01group;
+
+/**
+ * This is Animal abstract class
+ *
+ * @author Sreebharath Mallampati
+ */
+public abstract class Animal {
+
+    //for name
+    protected final String name;
+
+    /**
+     * This is Animal constructor
+     *
+     * @author Sreebharath Mallampati
+     * @param name
+     */
+    Animal(String name) {
+        this.name = name;
+    }
+
+    /**
+     * This is a speak method
+     *
+     * @author Sreebharath Mallampati
+     */
+    public void speak() {
+        System.out.println("Generic animal sound");
+    }
+
+    /**
+     * This is a move method which gives the moving nature.
+     *
+     * @author Sreebharath Mallampati
+     */
+    public void move() {
+        System.out.println("Generic animal movement");
+    }
--- src/main/java/edu/nwmissouri/zoo01group/ZooCostCalculatorApp.java
+++ src/main/java/edu/nwmissouri/zoo01group/ZooCostCalculatorApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZooCostCalculatorApplication {
+public class ZooCostCalculatorApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZooCostCalculatorApplication.class, args);
+		SpringApplication.run(ZooCostCalculatorApp.class, args);
 	}
 
 }
