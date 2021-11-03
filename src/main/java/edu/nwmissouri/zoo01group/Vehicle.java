944a64a083cec3accda6a95c1538b99de598c052
akhilreddy097 <akhilreddym970@gmail.com>
2021-11-02 22:29:09 -0500

    Added Vehicle.java and FourWheeler.java

---
+++ src/main/java/edu/nwmissouri/zoo01group/Vehicle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo01group;
+
+/**
+ *
+ * @author S545012
+ */
+public abstract class Vehicle {
+    
+    public double getMileage(){
+return 0;
+}
+
+public double getMaxSpeed(){
+return 0;
+}
+    
+}
