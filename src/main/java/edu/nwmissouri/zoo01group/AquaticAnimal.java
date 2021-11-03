2c9a37246443bf1f4ab8eeacae9b18d140484d47
anuragkolluru <anuragcool.kolluru87@gmail.com>
2021-11-02 21:43:16 -0500

    Added AquaticAnimal.java and TerrestrialAnimal.java

---
+++ src/main/java/edu/nwmissouri/zoo01group/AquaticAnimal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo01group;
+
+/**
+ *
+ * @author Anurag Kolluru
+ * 
+ */
+public class AquaticAnimal extends Animal{
+
+    public AquaticAnimal(String name) {
+        super(name);
+    }
+    
+}
