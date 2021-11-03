773db1e3f79810d0081b6f82e4efffaded56ba24
bharathnw <msreebharath21@gmail.com>
2021-11-02 21:04:32 -0500

    added springboot initializr

---
+++ src/main/java/edu/nwmissouri/zoo01group/ZooCostCalculatorApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo01group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooCostCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooCostCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/zoo01group/ZooCostCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo01group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooCostCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
