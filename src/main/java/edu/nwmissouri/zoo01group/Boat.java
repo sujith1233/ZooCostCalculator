a26526caa0dde8e3724c5507291d75f49339b736
Govind2003 <89623058+Govind2003@users.noreply.github.com>
2021-11-05 23:54:30 -0500

    Added new methods

--- src/main/java/edu/nwmissouri/zoo01group/Boat.java
+++ src/main/java/edu/nwmissouri/zoo01group/Boat.java
@@ -6,11 +6,66 @@ package edu.nwmissouri.zoo01group;
 
 /**
  *
- * @author S545524
+ * @author Pandiri Govinda Reddy
  */
 public class Boat extends Vehicle{
+    private int speed;
+    private String company;
+    private double length;
+    /**
+     * getter method to get speed 
+     * @return speed
+     
+    
+    */
+    public int getSpeed() {
+        return speed;
+    }
+    /**
+     * setter method to get speed
+     * @return speed
+    */
 
-    public Boat() {
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    /**
+     *getter method to get company 
+     * @ return company
+    */
+
+    public String getCompany() {
+        return company;
+    }
+    /**
+     * setter method to get company
+     * @return company
+    */
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+    /**
+     * Method used to display company of boat
+   
+    */
+    public String boatMake(){
+        return this.company;
+    }
+    /**
+     *  Method used to show speed of the boat 
+    */
+    public double milesPerHours(){
+        return this.speed;
     }
     
 }
+
--- src/main/java/edu/nwmissouri/zoo01group/Corsain.java
+++ src/main/java/edu/nwmissouri/zoo01group/Corsain.java
@@ -6,11 +6,44 @@ package edu.nwmissouri.zoo01group;
 
 /**
  *
- * @author S545524
+ * @author Pandiri Govinda Reddy
  */
 public class Corsain extends Boat {
+    private double length;
+    private double cost;
+  /**
+   * getter and setter to get length and cost
+   * return length
+    
+    */
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 
     public Corsain() {
+       
     }
+     
     
+    public double size(){
+        return this.length;
+    }
+    /**
+     * Method used to calculate the cost
+    */
+    public double costPerMile(){
+        return this.cost;
+    }
 }
