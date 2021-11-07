60f3845ff0ac8b1cd7278657f50b35b03a1758ec
bharathnw <msreebharath21@gmail.com>
2021-11-06 20:01:44 -0500

    added index.html

---
+++ src/main/java/edu/nwmissouri/zoo01group/ZooCostCalculatorController.java
@@ -0,0 +1,46 @@
+package edu.nwmissouri.zoo10group;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+import java.lang.reflect.Method;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Controller that handles the default request ("/").
+ *
+ * Use the new Java 15 text blocks to provide our menu. Thymeleaf:
+ * https://spring.io/guides/gs/serving-web-content/
+ *
+ * @author Alex Dieringer
+ */
+@Controller
+public class ZooCostCalculatorController {
+
+    /**
+     * The @GetMapping annotation ensures that HTTP GET requests to / are mapped
+     * to the index() method.Example:
+     * https://spring.io/guides/gs/serving-web-content/
+     *
+     *
+     * @param idParam - the id provided in the URL
+     * @param model - the model that holds information from controller to view
+     * @return
+     */
+    @GetMapping(path = "/")
+    public String index(
+            @RequestParam(name = "id", required = false, defaultValue = "0") String idParam,
+            Model model) {
+        model.addAttribute("id", idParam);
+        return "index";
+    }
+
+}
+
