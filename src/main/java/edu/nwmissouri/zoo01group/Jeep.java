aa64047aa83b8ecd3b8eefa399633caa2bd2dee7
NaveenKanaparthi <S545398@NWMISSOURI.EDU>
2021-11-06 22:06:16 -0500

    changes in Land rover

--- src/main/java/edu/nwmissouri/zoo01group/Jeep.java
+++ src/main/java/edu/nwmissouri/zoo01group/Jeep.java
@@ -9,9 +9,66 @@ package edu.nwmissouri.zoo01group;
  * @author S545398
  */
 public class Jeep extends FourWheeler {
-
+    private String engineTypeandRequiredFuel ;
+    private double Displacement;
+    private String fuelSystem;
     public Jeep() {
-    }
-    
+    }/**
+     * 
+     * @return engineTypeandRequiredFuel // 
+     */
+
+    public String getEngineTypeandRequiredFuel() {
+        return engineTypeandRequiredFuel;
+    }/**
+     * 
+     * @param engineTypeandRequiredFuel //
+     */
+
+    public void setEngineTypeandRequiredFuel(String engineTypeandRequiredFuel) {
+        this.engineTypeandRequiredFuel = engineTypeandRequiredFuel;
+    }/**
+     * 
+     * @return Displacement
+     */
+
+    public double getDisplacement() {
+        return Displacement;
+    }/**
+     * 
+     * @param Displacement // (litres per cubic inches)
+     */
+
+    public void setDisplacement(double Displacement) {
+        this.Displacement = Displacement;
+    }/**
+     * 
+     * @return fuelSystem
+     */
+
+    public String getFuelSystem() {
+        return fuelSystem;
+    }/**
+     * 
+     * @param fuelSystem // kinda fuel system
+     */
+
+    public void setFuelSystem(String fuelSystem) {
+        this.fuelSystem = fuelSystem;
+    }/**
+     * 
+     * @return maximumHorsePower // maximum horsepower of engine
+     */
+   public String maxHorsePower(){
+       String maximumHorsePower="270@5250";
+       return maximumHorsePower;
+   }/**
+    * 
+    * @return maxTorque // maximum torque
+    */
+   public String maximumTorque(){
+       String maxTorque="260@4800";
+       return maxTorque;
+   }
     
 }
--- src/main/java/edu/nwmissouri/zoo01group/LandRover.java
+++ src/main/java/edu/nwmissouri/zoo01group/LandRover.java
@@ -9,8 +9,79 @@ package edu.nwmissouri.zoo01group;
  * @author S545398
  */
 public class LandRover extends FourWheeler{
-
+     private double price;
+     private String fuelType;
+     private int noOfCylinder;
     public LandRover() {
-    }
+    }/**
+     * 
+     * @param price // price of land rover
+     * @param fuelType // type of the fuel vehicle runs on
+     * @param noOfCylinder // no of cylinder
+     */
+
+    public LandRover(double price, String fuelType, int noOfCylinder) {
+        this.price = price;
+        this.fuelType = fuelType;
+        this.noOfCylinder = noOfCylinder;
+    }/**
+     * 
+     * @return price // price of land rover
+     */
+
+    public double getPrice() {
+        return price;
+    }/**
+     * 
+     * @param price // price of land rover
+     */
+
+    public void setPrice(double price) {
+        this.price = price;
+    }/**
+     * 
+     * @return fuelType // type of the fuel vehicle runs on
+     */
+
+    public String getFuelType() {
+        return fuelType;
+    }/**
+     * 
+     * @param fuelType  // type of the fuel vehicle runs on
+     */
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }/**
+     * 
+     * @return noOfCylinder // no of cylinder
+     */
+
+    public int getNoOfCylinder() {
+        return noOfCylinder;
+    }/**
+     * 
+     * @param noOfCylinder // no of cylinder
+     */
+
+    public void setNoOfCylinder(int noOfCylinder) {
+        this.noOfCylinder = noOfCylinder;
+    }/**
+     * 
+     * @return maxPower // max power of the engine 
+     */
     
+    public String maxPower(){
+        String maxPower ="557.86bhp@6000-6500rpm";
+        
+        return maxPower;
+    }/**
+     * 
+     * @return  maxTorque // maxTorque of the engine
+     */
+    public String maxTorque(){
+        String maxTorque="700Nm@3500-5000rpm";
+        return maxTorque;
+    }
 }
+
